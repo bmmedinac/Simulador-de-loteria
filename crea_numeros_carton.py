cc190941ca9e5d7d48438ddcd2ed674f00483ae1
Basthian Medina <bmmedina@uc.clgit>
2021-02-12 13:19:55 -0300

    carton creable

---
+++ crea_numeros_carton.py
@@ -0,0 +1,45 @@
+import numpy as np
+from random import randint
+
+def rellenar_numeros_columnas():
+    numeros_totales = 0
+    columna_no_vacia = True
+    while numeros_totales != 15 and columna_no_vacia == True:
+    # Asigna entre 1 a 3 números aleatorios en un rango a cada columna
+        c1 = randint(1, 3)
+        primera_columna = list(np.random.choice(a = range(1,9), size = c1, replace = False))
+        
+        c2 = randint(1, 3)
+        segunda_columna = list(np.random.choice(a = range(10,19), size = c2, replace = False)) 
+        
+        c3 = randint(1, 3)
+        tercera_columna = list(np.random.choice(a = range(20,29), size = c3, replace = False))
+        
+        c4 = randint(1, 3) 
+        cuarta_columna = list(np.random.choice(a = range(30,39), size = c4, replace = False))
+        
+        c5 = randint(1, 3)
+        quinta_columna = list(np.random.choice(a = range(40,49), size = c5, replace = False))
+        
+        c6 = randint(1, 3)
+        sexta_columna = list(np.random.choice(a = range(50,59), size = c6, replace = False))
+        
+        c7 = randint(1, 3)
+        septima_columna = list(np.random.choice(a = range(60,69), size = c7, replace = False))
+        
+        c8 = randint(1, 3)
+        octava_columna = list(np.random.choice(a = range(70,79), size = c8, replace = False))
+        
+        c9 = randint(1, 3)
+        novena_columna = list(np.random.choice(a = range(80,90), size = c9, replace = False))
+        
+        numeros_carton_particular = primera_columna + segunda_columna + tercera_columna + cuarta_columna + quinta_columna + sexta_columna + septima_columna + octava_columna + novena_columna
+        # Registra la cantidad de números añadidos al cartón
+        numeros_totales = int(c1) + int(c2) + int(c3) + int(c4) + int(c5) + int(c6) + int(c7) + int(c8) + int(c9)
+        
+        # Registra si existe al menos una columna con valor 0/Falso
+        no_cero = [len(i) for i in [primera_columna, segunda_columna, tercera_columna, cuarta_columna, quinta_columna, sexta_columna, septima_columna, octava_columna, novena_columna]]
+        columna_vacia = all(no_cero)
+        
+        if numeros_totales == 15 and columna_no_vacia == True:
+            return numeros_carton_particular
---
+++ juego-lota.py
@@ -0,0 +1,67 @@
+##### HEAD ######
+
+import numpy as np
+from random import randint
+from os import system
+from crea_numeros_carton import *
+
+programa_titulo = "Loteria - tu lota de la playa"
+system("mode 110, 30")
+system("Título: " + programa_titulo)
+
+def clear():
+    system('clear')
+    
+####### INICIO DEL JUEGO ####
+clear()
+print("Lotería - Tu lota de la playa")
+
+def intro():
+    print("")
+    print("¡Pase, caserita, caserito! ¡Juéguese una lota!")
+    print("¡Llévese un microondas, unas papitas pa' picar, todo con su lota!")
+    print("")
+    print("Tóme, un cartón de cortesia de la casa, porque me cayó bien...")
+    input("Aprieta Enter para recibir el cartón que te regaló el buen hombre...")
+
+intro()
+
+print("Estamos en la playa, pero la tecnología no falta. Los números de tu cartón se generan automáticamente.")
+input("Aprieta Enter para generar un cartón.")
+
+numeros_carton_elegido = None
+
+def crear_carton():
+    global numeros_carton_elegido
+    clear()
+    print("¿Te gusta estos números?")
+    print("")
+    oferta_numeros = rellenar_numeros_columnas()
+    print(oferta_numeros)
+    print("")
+    
+    while numeros_carton_elegido is None:
+        eleccion_carton = input("""
+        
+        Si te gustaron estos números, presiona 1. Si quieres otro cartón, presiona 2. Si quieres irte, presiona 0
+                                
+        > """)
+        if eleccion_carton == "1":
+            clear()
+            print("¡A JUGAR! Tus números son:")
+            numeros_carton_elegido = oferta_numeros
+            print(numeros_carton_elegido)
+        elif eleccion_carton == "2":
+            clear()
+            print("¿Y este cartón?")
+            print("")
+            oferta_numeros = rellenar_numeros_columnas()
+            print(oferta_numeros)
+        elif eleccion_carton == "0":
+            clear()
+            print("¡Vuelve pronto!")
+            break
+        else:
+            print("Ingresa una opción válida")
+
+crear_carton()
